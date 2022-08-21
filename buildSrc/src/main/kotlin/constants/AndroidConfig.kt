ff7c54f34410e6205658758caa439c0c2119fad5
Julian Parrilla <jparrilla@fintonic.com>
2022-08-21 18:30:05 +0200

    UI-Working

--- buildSrc/src/main/kotlin/constants/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/constants/AndroidConfig.kt
@@ -1,9 +1,15 @@
 object AndroidConfig {
     const val COMPILE_SDK_VERSION = 32
     const val MIN_SDK_VERSION = 27
+<<<<<<< develop
     const val TARGET_SDK_VERSION = 32
     const val VERSION_CODE = 2
     const val VERSION_NAME = "1.0"
+=======
+    const val TARGET_SDK_VERSION = 30
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "0.1"
+>>>>>>> UI-Working
 
     const val ID = "com.julianparrilla.dragonbooker"
     const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
